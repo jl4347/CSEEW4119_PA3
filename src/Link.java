f0a3e7412076bf4338ce8d398c14b30c1934a884
JIALUN LIU <jl4347@columbia.edu>
2015-12-17 01:27:04 -0500

    Link: timestamp to record the last update time

--- src/Link.java
+++ src/Link.java
@@ -3,12 +3,13 @@ public class Link {
 	public int listenPort;
 	public double weight;
 	public boolean isAlive = true;
+	public long lastUpdate;
 	
-	public Link(String routerIP, int listenPort, double weight) {
-		super();
+	public Link(String routerIP, int listenPort, double weight, long lastUpdate) {
 		this.routerIP = routerIP;
 		this.listenPort = listenPort;
 		this.weight = weight;
+		this.lastUpdate = lastUpdate;
 	}
 
 	@Override
